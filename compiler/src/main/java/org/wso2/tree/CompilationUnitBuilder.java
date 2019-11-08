1d17fae2cac6e22c9145f825040220ff5cca4c24
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2019-11-08 15:26:03 +0530

    refactor: inject a compilatinUnit reference
    Pass a CompilationUnit to the CompilationUnitBuilder so that the resulting object can be accessed afterwards. Plus minor improvments

--- compiler/src/main/java/org/wso2/tree/CompilationUnitBuilder.java
+++ compiler/src/main/java/org/wso2/tree/CompilationUnitBuilder.java
@@ -1,17 +1,19 @@
 package org.wso2.tree;
 
-import org.wso2.antlr.CompilerParser;
-
 import java.util.Stack;
+import org.wso2.antlr.CompilerParser;
 
 public class CompilationUnitBuilder {
     private CompilationUnit compilationUnit;
-    private Stack<Node> nodes = new Stack<>();
     private Stack<Client> clientStack = new Stack<>();
     private Stack<KeyValuePair> keyValuePairStack = new Stack<>();
 
+    public CompilationUnitBuilder(CompilationUnit compilationUnit) {
+        this.compilationUnit = compilationUnit;
+    }
+
     public void startCompilationUnit(CompilerParser.CompilationUnitContext ctx) {
-        compilationUnit = new CompilationUnit();
+
     }
 
     public void endCompilationUnit(CompilerParser.CompilationUnitContext ctx) {
